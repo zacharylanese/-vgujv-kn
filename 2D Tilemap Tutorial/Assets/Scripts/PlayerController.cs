2ca79bcbd3100b68e570f2b3e375dc4c06c46284
Zachary Lanese <zacharylanese@gmail.com>
2019-07-15 17:05:13 -0400

    tilemap tutorial push 1

---
+++ 2D Tilemap Tutorial/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    public float speed;
+    public float jumpForce;
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        Vector2 movement = new Vector2(moveHorizontal, 0);
+        rb2d.AddForce(movement * speed);
+        if (Input.GetKey("escape"))
+        Application.Quit(); 
+    }
+    void OnCollissionStay2D(Collision2D collission)
+    {
+        if(collission.collider.tag == "Ground")
+        {
+            if(Input.GetKey(KeyCode.UpArrow))
+            {
+                rb2d.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            }
+        }   
+    }
+}
